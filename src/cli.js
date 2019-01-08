605f9c285369b373845edffda18ea237a3cb856d
Wade Penistone <wadespackages@yahoo.com>
2019-01-08 19:27:26 +0000

    CLI boilerplate from gatsby

--- src/cli.js
+++ src/cli.js
@@ -1,2 +1,46 @@
 #!/usr/bin/env node
-console.log('CLI linked');
+const yargs = require('yargs')
+
+const handlerP = fn => (...args) => {
+  Promise.resolve(fn(...args)).then(
+    () => process.exit(0),
+    err => report.panic(err)
+  )
+}
+
+// Base
+let cli = yargs()
+cli
+  .usage(`Usage: $0 <command> [options]`)
+  .alias(`h`, `help`)
+  .alias(`v`, `version`)
+  .option(`verbose`, {
+    default: false,
+    type: `boolean`,
+    describe: `Turn on verbose output`,
+    global: true,
+  })
+  .option(`no-color`, {
+    default: false,
+    type: `boolean`,
+    describe: `Turn off the color in output`,
+    global: true,
+  })
+
+// Commands
+return cli
+      .command({
+        command: `new [rootPath] [starter]`,
+        desc: `Create new Lowbot project.`,
+        handler: handlerP(
+          ({ rootPath, starter = `lowbot/lowbot-starter-default` }) => {
+            console.log('input', rootPath, starter);
+          }
+        ),
+      })
+      .wrap(cli.terminalWidth())
+      .demandCommand(1, `Pass --help to see all available commands and options.`)
+      .strict()
+      .showHelpOnFail(true)
+      .recommendCommands()
+      .parse(process.argv.slice(2))
