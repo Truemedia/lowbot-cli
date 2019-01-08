1b3c888cc08310a2af284bdf75becc06e9d0d433
Wade Penistone <wadespackages@yahoo.com>
2019-01-08 20:05:19 +0000

    Stack prompt

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,23 @@
 #!/usr/bin/env node
 const yargs = require('yargs')
+const inquirer = require('inquirer')
+const report = require('signale')
 const initStarter = require('./init-starter')
 
-const handlerP = fn => (...args) => {
-  Promise.resolve(fn(...args)).then(
+const q = fn => (...args) => {
+  return inquirer.prompt([
+    {
+      name: 'stack',
+      type: 'list',
+      message: 'Which JAMstack are you using to power the bot as a data service?',
+      choices: ['Gatsby', 'Gridsome']
+    }
+  ]).then(answers => {
+    console.log('stack', answers.stack);
+    return fn(...args)
+  }).then(
     () => process.exit(0),
-    err => report.panic(err)
+    err => report.error(err)
   )
 }
 
@@ -33,11 +45,16 @@ return cli
       .command({
         command: `new [rootPath] [starter]`,
         desc: `Create new Lowbot project.`,
-        handler: handlerP(
+        handler: q(
           ({ rootPath, starter = `lowbot/lowbot-starter-default` }) => {
             return initStarter(starter, { rootPath })
           }
-        ),
+        )
+        // handler: handlerP(
+        //   ({ rootPath, starter = `lowbot/lowbot-starter-default` }) => {
+        //     return initStarter(starter, { rootPath })
+        //   }
+        // ),
       })
       .wrap(cli.terminalWidth())
       .demandCommand(1, `Pass --help to see all available commands and options.`)
