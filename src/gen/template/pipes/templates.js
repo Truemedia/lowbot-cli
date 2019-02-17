a9aba816974443cb59c5fa589c38f7f4b2e8bfec
Wade Penistone <wadespackages@yahoo.com>
2019-02-17 16:04:48 +0000

    WIP

--- src/gen/template/pipes/templates.js
+++ src/gen/template/pipes/templates.js
@@ -8,6 +8,12 @@ class TemplateFiles extends File
     super(opts);
   }
 
+  json(destPath, doc)
+  {
+    let fileContents = JSON.stringify(doc);
+    return lazypipe().pipe( this.pre(destPath, fileContents) );
+  }
+
   get render()
   {
     let {tplName} = this.opts;
@@ -15,11 +21,15 @@ class TemplateFiles extends File
     return lazypipe()
       .pipe( this.src(__dirname, '*.hbs') )
       .pipe( this.tpl(this.opts) )
+      .pipe( this.json('./test.json', {hello: 'world'}))
       .pipe(this.rename, (file) => { // Directory and filename
         if (file.basename.includes('ssml')) {
           file.dirname = './speech';
         } else if (file.basename.includes('body')) {
           file.dirname = './display';
+        }
+        if (file.basename.includes('test')) {
+          file.dirname = './';
         }
         file.basename = file.basename.replace('template', tplName);
       });
--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,7 +1,9 @@
+// const adapter = require('./gen/adapter/adapter');
 const locale = require('./gen/locale/locale');
 const resolver = require('./gen/resolver/resolver');
 const template = require('./gen/template/template');
 
+// exports.adapter = adapter;
 exports.locale = locale;
 exports.resolver = resolver;
 exports.template = template;
