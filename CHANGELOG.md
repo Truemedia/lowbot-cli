3d9b8c00fed0a950b63c316acae1eb3e3cb630dd
Wade Penistone <wadespackages@yahoo.com>
2019-03-27 19:55:04 +0000

    Python version and run command

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,11 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.1.3] - 2019-03-27
+### Added
+- Python version flag (2 or 3)
+- Run command
+
 ## [0.1.2] - 2019-02-17
 ### Added
 - NLU commands
--- src/cli.js
+++ src/cli.js
@@ -27,11 +27,27 @@ const q = fn => (...args) => {
   )
 }
 
+// Python namespace on CLI
+function py(version)
+{
+  let cmd = null
+  switch (version) {
+    case 3:
+      cmd = 'python3'
+    break
+    default:
+      cmd = 'python'
+    break
+  }
+  return cmd
+}
+
 // Base
 let cli = yargs()
 cli
   .usage(`Usage: $0 <command> [options]`)
   .alias(`h`, `help`)
+  .alias(`py`, `python`)
   .alias(`v`, `version`)
   .option(`verbose`, {
     default: false,
@@ -45,10 +61,34 @@ cli
     describe: `Turn off the color in output`,
     global: true,
   })
+  .option(`python`, {
+    default: null,
+    type: 'number',
+    describe: `Python version to use (2 or 3)`,
+    global: true
+  })
 
 // Commands
 return cli
-      .command({
+      .command({ // Learn intents
+        command: `learn intents`,
+        desc: `Train bot to recognise intents`,
+        handler: (argv) => {
+          console.log('Training bot to recognise intents...');
+          let python = py(argv.py);
+          execute(`${python} -m rasa_nlu.train -c nlu_config.yml --data nlu.md -o models --fixed_model_name nlu --project current --verbose`, function(output) {
+            console.log(output);
+          });
+        }
+      })
+      .command({ // Generate development files
+        command: `make [generator]`,
+        desc: `Generate files for extending Mind Stack functionality`,
+        handler: ({generator}) => {
+          return generators[generator]()
+        }
+      })
+      .command({ // Create new bot instance
         command: `new [rootPath] [starter]`,
         desc: `Create a new Mind Stack project.`,
         handler: q(
@@ -62,30 +102,23 @@ return cli
         //   }
         // ),
       })
-      .command({
-        command: `make [generator]`,
-        desc: `Generate files for extending Mind Stack functionality`,
-        handler: ({generator}) => {
-          return generators[generator]()
-        }
-      })
-      .command({
-        command: `learn intents`,
-        desc: `Train bot to recognise intents`,
-        handler: () => {
-          console.log('Training bot to recognise intents...');
-          execute('python -m rasa_nlu.train -c nlu_config.yml --data nlu.md -o models --fixed_model_name nlu --project current --verbose', function(output) {
+      .command({ // Launch NLU pipeline
+        command: `pipeline nlu`,
+        desc: `Run NLU pipeline`,
+        handler: (argv) => {
+          console.log('Running Rasa server...');
+          let python = py(argv.py);
+          execute(`${python} -m rasa_nlu.server --path models/`, function(output) {
             console.log(output);
           });
         }
       })
-      .command({
-        command: `pipeline nlu`,
-        desc: `Run NLU pipeline`,
+      .command({ // Run a bot instance
+        command: `run`,
+        desc: `Run a bot instance`,
         handler: () => {
-          console.log('Running Rasa server...');
-          execute('python -m rasa_nlu.server --path models/', function(output) {
-            console.log(output);
+          execute('node index.js', function(output) {
+            process.stdout.write(output);
           });
         }
       })
